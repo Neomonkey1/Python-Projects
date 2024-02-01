125e39f918c24f6c28d50762b0f9ff15c42b3598
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-01 10:56:00 -0500

    Create dataTypes.py
    list of Python datatypes added comments

---
+++ dataTypes.py
@@ -0,0 +1,27 @@
+# string
+A = 'String'
+# int
+B = 10
+# float
+C = 15.15
+# list
+D = ['apple', 'banana', 'cherry']
+# tuple
+E = ('apple', 'banana', 'cherry')
+# dictionary
+F = {'name': 'Ray', 'age': 42}
+# set
+G = {'apple', 'banana', 'cherry'}
+# boolean
+H = True
+I = False
+# if else statements
+if B > C:
+    print('B is greater than C')
+else:
+    print('B is not greater than C')
+
+if B < C:
+    print('B is less than C')
+else:
+    print('B is not less than C')
