824313f0a9c131d04222fdb6a84a843502ab5436
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-01 20:21:02 -0500

    Update printNum.py
    added the random module added comments

--- printNum.py
+++ printNum.py
@@ -1,10 +1,14 @@
 
 import ourModule
 import datetime
+import random
 
+# This part uses ourModule
 if __name__ == '__main__':
   results = ourModule.getNumbers(5,9)
   print(results)
-
+# This uses the datetime module
 A = datetime.datetime.now()
 print(A)
+# This uses the random module
+print(random.randrange(1,100))
