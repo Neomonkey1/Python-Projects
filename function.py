84fb84329304123b1983750a35e942d07e173124
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-02 20:50:20 -0500

    Update function.py
    added functions
    added comments

--- function.py
+++ function.py
@@ -28,3 +28,34 @@ def get_name():
 
 
 get_name()
+
+# made and called a function
+def timesTwo(x,y):
+    ans = x * y
+    print(ans)
+
+timesTwo(5,9)
+
+# using lambda
+z = lambda a: a * 10
+print(z(8))
+
+# using the format()
+fname = 'Holly'
+lname = 'Poole'
+print('Hello {} {}, welcome to Python!'.format(fname, lname) )
+
+# using more format()
+print('binary: {0:b}, decimal: {0:x}, percentage: {0:%}'.format(5))
+
+# using a function in an variable
+def getSum(num1, num2):
+    answer = num1 + num2
+    print('The answer is {}.'.format(answer))
+getSum(2,4)
+myAdd = getSum
+myAdd(2,4)
+
+# using the type()
+print(type(3))
+print(type('hello'))
