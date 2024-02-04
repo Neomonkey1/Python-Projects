f72cb67299e6d6088a9d235879db1cf9a8da46a2
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-03 20:31:01 -0500

    Create myQuiz.py
    imported from Budget.py to use function
    added comments

---
+++ myQuiz.py
@@ -0,0 +1,6 @@
+#Statement C 
+import Budget; print(Budget.calcBills())
+
+#statement A import Budget does not work
+#statement B import Budget; print(calcBills()) does not work
+#statement D print(Budget.calcBills()) does not work
