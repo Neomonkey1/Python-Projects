abdb3c864f0c7dcd86d776a00bd0ff19ecbac52c
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-03 20:07:58 -0500

    Create commentTest.py
    added function
    added comments

---
+++ commentTest.py
@@ -0,0 +1,16 @@
+
+# This is my note.
+
+""" This is also my note
+and this is still my note
+and this again is still my note.
+"""
+## Using alt + 3 to make this a comment it
+
+def printMe():
+    """This is my docstring
+    """
+
+    print('Hey I left a docstring!')
+
+print(printMe.__doc__)
