6cfea8a2d29a1f175df8d6fad5c3fae7d9a889e2
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-04 21:28:50 -0500

    Create NiceOrMean.py
    added comments (version, author and purpose)
    added functions

---
+++ NiceOrMean.py
@@ -0,0 +1,39 @@
+#
+#Python:    3.11.7
+#
+#Author:    Raymond Harrison
+#
+#Purpose:   The Tech Academy - Python Course, Creating our first program together.
+#           Demonstration how to pass variables from function to function
+#           while producing a functional game.
+#
+#           Reminder, function_name(variable) _means that we pass in the variable.
+#           return variable _means that we are returning the variable to
+#           back to the calling function.
+
+
+def start ():
+    f_name = 'Sarah'
+    l_name = 'Connor'
+    age = 28
+    gender = 'Female'
+    get_info(f_name, l_name, age, gender)
+
+
+def get_info(f_name,l_name,age,gender):
+    print('My name is {} {}. I am {} year old {}.'.format(f_name,l_name,age,gender))
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    start()
