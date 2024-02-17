b4db93d4d6adf9f405025f5ae142b8fd53334c48
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-02-16 22:42:59 -0500

    Update web_page_generator.py
    added button and label
    added function
    added comments

--- web_page_generator.py
+++ web_page_generator.py
@@ -15,13 +15,19 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-        # Creates button for Default HTML page
+        # Create button for Default HTML page
         self.btn = Button(self.master, text="Default HTML Page", width = 30, height = 2, command=self.defaultHTML)
-        self.btn.grid(row=3, column=3, padx=(10,10), pady=(10,10))
+        self.btn.grid(row=3, column=1, padx=(10,10), pady=(10,10))
 
-        # Creates button for Custom Text to HTML page
-        self.btn = Button(self.master, text="Submit Custom Text", width = 30, height = 2)
-        self.btn.grid(row=3, column=4, padx=(10,10), pady=(10,10))
+        # Create button for Custom Text to HTML page
+        self.btn = Button(self.master, text="Submit Custom Text", width = 30, height = 2, command=self.customHTML)
+        self.btn.grid(row=3, column=2, padx=(10,10), pady=(10,10))
+
+        # Create input and Label text
+        self.lbl_customText = tk.Label(self.master, text="Enter custom text or click the Default HTML page button.")
+        self.lbl_customText.grid(row=0, column=0, padx=(30,10), pady=(10,10))
+        self.customText = Entry(width = 130)
+        self.customText.grid(row=1, column=0, columnspan=4)
 
     # Creating function to make HTML
     def defaultHTML(self):        
@@ -33,6 +39,14 @@ class ParentWindow(Frame):
         webbrowser.open_new_tab("index.html")
 
 
+    # Creating function to make custom text HTML
+    def customHTML(self):
+        custom_HtmlText = self.customText.get()
+        htmlFile = open("index.html","w")
+        htmlCustom = "<html>\n<body>\n<h1>" + custom_HtmlText + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlCustom)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
 
 
 
